2465db2759844d1aa593a167a42da8797b2f5403
siriusg2 <gibsonavilan@gmail.com>
2023-12-16 10:51:54 -0500

    logo animations added, href projects added

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,12 @@
 FROM node:lts AS runtime
-RUN npm i -g pnpm
 WORKDIR /app
-
+RUN npm install -g pnpm
 COPY . .
 
 RUN pnpm install
 RUN pnpm run build
-RUN pnpm astro add node -y
 
 ENV HOST=0.0.0.0
 ENV PORT=4321
 EXPOSE 4321
-CMD [ "pnpm", "run", "dev" ]
+CMD node ./dist/server/entry.mjs
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -13,57 +13,63 @@ const ProjectCard = () => {
           heading="Space food"
           description="This is a full stack application, with advanced features, such as third-party authentication (google login), payment gateway, sending email notifications, among others! It was built using the PERN stack (Postgresql, Expressjs, React, Node), which emulates the operation of a fast food e-commerce "
           imgSrc={spacefood}
+          url={"https://spacefood.netlify.app/"}
         />
         <Card
           heading="Store space food"
           description="This is a full stack application emulates the operation of a fast food e-commerce from administration side "
           imgSrc={storespacefood}
+          url={"https://storespacefood.netlify.app/"}
         />
         <Card
           heading="Gamersland"
           description="This is a full-stack application based on the theme of retro videogames"
           imgSrc={gamersland}
+          url={"https://gamersland.netlify.app/"}
         />
         <Card
           heading="Multiverseland"
           description="This is a React application, based on the theme of the animated series Rick & Morty, which shows a series of cards with the basic information of each character in the serie"
           imgSrc={multiverseland}
+          url={"https://multiverseland.netlify.app/home"}
         />
       </div>
     </div>
   );
 };
 
-const Card = ({ heading, description, imgSrc }) => {
+const Card = ({ heading, description, imgSrc, url }) => {
   return (
-    <motion.div
-      transition={{
-        staggerChildren: 0.035,
-      }}
-      whileHover="hover"
-      className="w-full h-64 bg-slate-300 overflow-hidden cursor-pointer group relative"
-    >
-      <div
-        className="absolute inset-0 saturate-100 md:saturate-0 md:group-hover:saturate-100 group-hover:scale-110 transition-all duration-500 "
-        style={{
-          backgroundImage: `url(${imgSrc})`,
-          backgroundSize: "cover",
-
-          backgroundPosition: "center",
+    <a href={url}>
+      <motion.div
+        transition={{
+          staggerChildren: 0.035,
         }}
-      />
-      <div className="p-4 relative z-20 h-full  hover:text-[#0eab29]  text-primary-80 group-hover:text-white transition-colors duration-500 flex flex-col justify-between">
-        <FiArrowRight className="text-3xl group-hover:-rotate-45 transition-transform duration-500 ml-auto" />
-        <div>
-          <h4>
-            {heading.split("").map((l, i) => (
-              <ShiftLetter letter={l} key={i} />
-            ))}
-          </h4>
-          <p>{description}</p>
+        whileHover="hover"
+        className="w-full h-64 bg-slate-300 overflow-hidden cursor-pointer group relative"
+      >
+        <div
+          className="absolute inset-0 saturate-100 md:saturate-0 md:group-hover:saturate-100 group-hover:scale-110 transition-all duration-500 "
+          style={{
+            backgroundImage: `url(${imgSrc})`,
+            backgroundSize: "cover",
+
+            backgroundPosition: "center",
+          }}
+        />
+        <div className="p-4 relative z-20 h-full  hover:text-[#0eab29]  text-primary-80 group-hover:text-white transition-colors duration-500 flex flex-col justify-between">
+          <FiArrowRight className="text-3xl group-hover:-rotate-45 transition-transform duration-500 ml-auto" />
+          <div>
+            <h4>
+              {heading.split("").map((l, i) => (
+                <ShiftLetter letter={l} key={i} />
+              ))}
+            </h4>
+            <p>{description}</p>
+          </div>
         </div>
-      </div>
-    </motion.div>
+      </motion.div>
+    </a>
   );
 };
 
