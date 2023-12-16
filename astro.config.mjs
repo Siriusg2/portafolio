5328da2e4739429dbe523f7a2458c7afc6a64621
siriusg2 <gibsonavilan@gmail.com>
2023-12-16 12:17:14 -0500

    try to deploy site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,18 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: netlify(),
   integrations: [tailwind(), react()],
   vite: {
     ssr: {
-      noExternal: ["react-icons"],
-    },
-  },
+      noExternal: ["react-icons"]
+    }
+  }
 });
